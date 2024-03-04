a22266a39d3ddd5e0763685bf628dfac68e8d8d0
1271989088@qq.com <1271989008@qq.com>
2024-03-04 16:50:31 +0800

    fix analysis bug

--- testbed/js_test.py
+++ testbed/js_test.py
@@ -45,3 +45,63 @@ for dev in devices:
         different_case(datas[dev], mode=m)
         print()
 
+'''
+## dev: android
+### mode: exe
+browser ['Firefox', 'Chrome', 'Edge']
+mean [0.2664101311816021, 0.4300557715964846, 0.30353409722191305]
+std [0.08078915202880856, 0.09421303582390592, 0.07303594453009171]
+case shootoutc/nestedloop is abnormal in browser Chrome, ratio 0.7378640776699029
+case shootoutc/except is abnormal in browser Firefox, ratio 0.6051421404682275
+case coyotebench/fftbench is abnormal in browser Chrome, ratio 0.1181455121875467
+case coyotebench/fftbench is abnormal in browser Edge, ratio 0.775200635235034
+
+### mode: init
+browser ['Firefox', 'Chrome', 'Edge']
+mean [0.01879435517383383, 0.970990323274091, 0.010215321552075097]
+std [0.05906735001284601, 0.07882352085834896, 0.02429804554144364]
+case shootoutc/lists is abnormal in browser Firefox, ratio 0.42154523081512413
+case shootoutc/lists is abnormal in browser Chrome, ratio 0.5712674323215751
+case polybench/doitgen is abnormal in browser Firefox, ratio 0.4928571428571429
+case polybench/doitgen is abnormal in browser Chrome, ratio 0.24357142857142858
+case polybench/doitgen is abnormal in browser Edge, ratio 0.26357142857142857
+
+## dev: iphone
+### mode: exe
+browser ['Chrome', 'Safari', 'Firefox']
+mean [0.3502809181347279, 0.31557766279454286, 0.334141419070729]
+std [0.05854278014469351, 0.03601826252285353, 0.035827488352902975]
+case shootoutc/objinst is abnormal in browser Safari, ratio 0.16666666666666666
+case benchmarkgame/fasta is abnormal in browser Chrome, ratio 0.5833333333333334
+case benchmarkgame/fasta is abnormal in browser Safari, ratio 0.19444444444444445
+case benchmarkgame/fasta is abnormal in browser Firefox, ratio 0.2222222222222222
+case polybench/gesummv is abnormal in browser Firefox, ratio 0.18181818181818182
+case linpack/linpackpc is abnormal in browser Chrome, ratio 0.7698967304043206
+case linpack/linpackpc is abnormal in browser Safari, ratio 0.11653408261567655
+case linpack/linpackpc is abnormal in browser Firefox, ratio 0.11356918698000293
+
+### mode: init
+browser ['Chrome', 'Safari', 'Firefox']
+mean [0.34986528478094714, 0.30826881613140206, 0.3418658990876513]
+std [0.09639130800671275, 0.07405607017626184, 0.0683217406545399]
+case benchmarkgame/nbody is abnormal in browser Safari, ratio 0.7319587628865979
+case benchmarkgame/partialsums is abnormal in browser Firefox, ratio 0.6386554621848739
+case mcgill/misr is abnormal in browser Chrome, ratio 0.643312101910828
+case polybench/gesummv is abnormal in browser Safari, ratio 0.6196319018404908
+case coyotebench/lpbench is abnormal in browser Chrome, ratio 0.7440476190476191
+case misc/pi is abnormal in browser Chrome, ratio 0.7991071428571429
+case misc/pi is abnormal in browser Firefox, ratio 0.10267857142857142
+case misc/flops is abnormal in browser Chrome, ratio 0.7724867724867724
+case misc/flops is abnormal in browser Firefox, ratio 0.1111111111111111
+
+## dev: macintel
+### mode: exe
+browser ['Chrome', 'Edge', 'Firefox', 'Safari']
+mean [0.20199677143559852, 0.3622051055822542, 0.15869350500290846, 0.27710461797923885]
+std [0.04524217909535858, 0.052743288061265096, 0.04315653360445969, 0.04811045650182415]
+
+### mode: init
+browser ['Chrome', 'Edge', 'Firefox', 'Safari']
+mean [0.4724706779783827, 0.16622127451057558, 0.2551719153563142, 0.10613613215472746]
+std [0.09069920073850857, 0.09208603124199806, 0.025967133713725155, 0.028365041220227722]
+'''
--- testbed/testing.py
+++ testbed/testing.py
@@ -21,22 +21,35 @@ def avg_init(log):
 
 def different_case(result, mode="exe"):
     ratio = {}
+    max_browser = max([len(result[case].keys()) for case in result])
     browindex = []
     for case in result:
+        if len(result[case].keys()) == max_browser:
+            browindex = list(result[case].keys())
+            break
+
+    for case in result:
         browser_vector = []
-        for browser in result[case]:
-            browindex.append(browser)
+        for browser in browindex:
+            if browser not in result[case]:
+                continue
             if mode == "exe":
-                browser_vector.append(avg_exe(result[case][browser]))
+                e = avg_exe(result[case][browser])
+                if e:
+                    browser_vector.append(e)
             elif mode == "init":
-                browser_vector.append(avg_init(result[case][browser]))
+                i = avg_init(result[case][browser])
+                if i:
+                    browser_vector.append(i)
         
         # normalize the vector
-        if browser_vector:
-            if len(browser_vector) < 3:
+        # if browser_vector not all zero or nan
+        if sum(browser_vector) != 0:
+            if len(browser_vector) < max_browser:
                 # print(f"case {case} has {len(browser_vector)} browsers")
                 continue
             ratio[case] = [x / sum(browser_vector) for x in browser_vector]
+
 
     # find abnormal case according to the ratio
     mean_ratio = []
@@ -47,7 +60,9 @@ def different_case(result, mode="exe"):
 
         std_i = np.std([x[i] for x in ratio.values()])
         std_ratio.append(std_i)
-    print(mean_ratio)
+    print(f"browser {browindex}")
+    print(f"mean {mean_ratio}")
+    print(f"std {std_ratio}")
     # print(std_ratio)
 
     ## find the abnormal case
@@ -76,7 +91,9 @@ if __name__ == "__main__":
 
 '''
 ## linux abnormal case, mode exe
-[0.32548098564573646, 0.33629646397615653, 0.3382225503781071]
+browser ['firefox', 'chrome', 'edge']
+mean [0.32548098564573646, 0.33629646397615653, 0.3382225503781071]
+std [0.043035128845624916, 0.022379201754107528, 0.024370384881946126]
 case benchmarkgame/puzzle is abnormal in browser edge, ratio 0.4238918704034983
 case benchmarkgame/fasta is abnormal in browser firefox, ratio 0.1330532212885154
 case benchmarkgame/fasta is abnormal in browser chrome, ratio 0.42156862745098045
@@ -85,7 +102,9 @@ case stanford/oscar is abnormal in browser firefox, ratio 0.18796992481203006
 case stanford/oscar is abnormal in browser chrome, ratio 0.4172932330827067
 
 ## linux abnormal case, mode init
-[0.5440382634590735, 0.22850325949933914, 0.22745847704158773]
+browser ['firefox', 'chrome', 'edge']
+mean [0.5440382634590735, 0.22850325949933914, 0.22745847704158773]
+std [0.10835683021723927, 0.09654488528250187, 0.09027735205605289]
 case shootoutc/sieve is abnormal in browser chrome, ratio 0.5443678160919541
 case stanford/queens is abnormal in browser edge, ratio 0.5510026155187445
 case stanford/bubblesort is abnormal in browser chrome, ratio 0.5929003021148036
@@ -94,7 +113,9 @@ case coyotebench/lpbench is abnormal in browser edge, ratio 0.5408022130013831
 case misc/dt is abnormal in browser chrome, ratio 0.5667963683527886
 
 ## windows abnormal case, mode exe
-[0.34653816584476227, 0.31243362137048275, 0.3410282127847549]
+browser ['edge', 'firefox', 'chrome']
+mean [0.34653816584476227, 0.31243362137048275, 0.3410282127847549]
+std [0.029840003871280402, 0.0533430466665759, 0.025862952611121096]
 case stanford/oscar is abnormal in browser edge, ratio 0.47484276729559743
 case stanford/oscar is abnormal in browser firefox, ratio 0.09433962264150943
 case stanford/oscar is abnormal in browser chrome, ratio 0.4308176100628931
@@ -104,7 +125,9 @@ case benchmarkgame/fasta is abnormal in browser firefox, ratio 0.090117152297987
 case benchmarkgame/fasta is abnormal in browser chrome, ratio 0.4532892760588766
 
 ## windows abnormal case, mode init
-[0.3264933659401144, 0.46973470047822125, 0.20377193358166468]
+browser ['edge', 'firefox', 'chrome']
+mean [0.3264933659401144, 0.46973470047822125, 0.20377193358166468]
+std [0.06262105074155215, 0.08060157853333877, 0.055236399658614946]
 case adobec/simple_types_loop_invariant is abnormal in browser firefox, ratio 0.20702402957486138
 case adobec/simple_types_loop_invariant is abnormal in browser chrome, ratio 0.5227356746765249
 case polybench/heat3d is abnormal in browser edge, ratio 0.02710266607747827
@@ -113,7 +136,9 @@ case polybench/heat3d is abnormal in browser chrome, ratio 0.016202680807188098
 case miscc/bigfib is abnormal in browser chrome, ratio 0.41869337148307106
 
 ## mac abnormal case, mode exe
-[0.2619497632926423, 0.2636348052152294, 0.2474183988252458, 0.22699703266688248]
+browser ['chrome', 'edge', 'firefox', 'safari']
+mean [0.2619497632926423, 0.2636348052152294, 0.2474183988252458, 0.22699703266688248]
+std [0.03646447462908403, 0.03573221516551143, 0.053884194108356855, 0.05500353940563542]
 case benchmarkgame/fasta is abnormal in browser chrome, ratio 0.4449536362467366
 case benchmarkgame/fasta is abnormal in browser edge, ratio 0.467913492427912
 case benchmarkgame/fasta is abnormal in browser firefox, ratio 0.05613685536677359
@@ -122,7 +147,9 @@ case miscc/stepanov_v1p2 is abnormal in browser chrome, ratio 0.4421960856982705
 case miscc/stepanov_v1p2 is abnormal in browser edge, ratio 0.3951848129958277
 
 ## mac abnormal case, mode init
-[0.1924478385626872, 0.21069834087484332, 0.39574288931141544, 0.20111093125105423]
+browser ['chrome', 'edge', 'firefox', 'safari']
+mean [0.1924478385626872, 0.21069834087484332, 0.39574288931141544, 0.20111093125105423]
+std [0.05393534959355746, 0.04693225232301523, 0.07161247370717962, 0.04533614217407997]
 case shootoutc/ackermann is abnormal in browser safari, ratio 0.3435114503816794
 case shootoutc/strcat is abnormal in browser edge, ratio 0.49288762446657186
 case shootoutc/ary is abnormal in browser safari, ratio 0.45417680454176806
